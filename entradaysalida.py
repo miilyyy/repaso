8c325a760342f4e311cb7dcc163c1f5c283bff19
miilyyy <mmilagrosvarela28@gmail.com>
2025-03-10 12:45:59 -0300

    Create entradaysalida.py

---
+++ entradaysalida.py
@@ -0,0 +1,7 @@
+crear un programa que solicite al usuario ingresar una palabra para luego mostrarlo por pantalla.
+a = input("Ingresa una palabra:  ")
+print("La palabra ingresada es:", a)
+hacer un programa que pida ingresar un numero entero, si el numero es mayor a 10 mostar por pantalla 10, si el numero es menor a 10 mostrar por pantalla fin del programa.
+numero = int("ingrese un numero entero:")
+if numero > 10 print("10")
+else print ("fin el programa")
